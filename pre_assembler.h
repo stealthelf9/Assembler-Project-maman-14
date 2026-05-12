c87305fb269b73c91373bd2440da26058377476e
Ariel Waldmann <waldmannariel@gmail.com>
2026-05-12 11:53:01 +0300

    added comments for pre_assembler.h

--- pre_assembler.h
+++ pre_assembler.h
@@ -3,6 +3,13 @@
 
 #include "macro_helpers.h"
 
+/**
+ * preAssembler
+ * This is the pre-assembler of the assembler. It expands macros and ommits the declarations from the output.
+ * @param file The .as file.
+ * @param file_name the filename of the .as file.
+ * @return 1 if there's an error, 0 if successful.
+ */
 int preAssembler(FILE *file, char *file_name);
 
 #endif
